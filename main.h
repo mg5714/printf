f0b69c4bce443de1423d52fad11ea070b5cdf576
Nadd99 <its.nadd99@gmail.com>
2023-10-15 05:09:43 +0200

    try it

--- main.h
+++ main.h
@@ -2,13 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 int _putchar(char c)
 void convertToBinary(unsigned int num);
 void printPointer(void *ptr);
-
+int printstr(va_list, arg_lst);
 int _printf(const char *format, ...);
 
 #endif
--- printf.c
+++ printf.c
@@ -38,6 +38,7 @@ int _printf (const char *format, ...)
 	    default:
 	      count += _putchar ('%');
 	      count += _putchar (*format);
+	      break;
 	    }
 	}
       else
