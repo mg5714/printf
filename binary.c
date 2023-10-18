99913abc8939550cf6c33065708e43f5cf406d81
Nadd99 <its.nadd99@gmail.com>
2023-10-18 02:09:05 +0200

    edit

--- binary.c
+++ binary.c
@@ -6,28 +6,25 @@
  */
 void convertToBinary(unsigned int num)
 {
-unsigned int binary[32];
+int size = 32;
+char *binary = malloc(sizeof(char) * size);
 int j, i = 0;
 
-if (num == NULL)
-{
-	return (-1);
-}
-if (num < 1)
+if (num == 0 || num < 0)
 {
 	return (-1);
 }
 
 while (num > 0)
 {
-binary[i] = num % 2;
+binary[i++] = num % 2 + '0' ;
 num = num / 2;
-i++;
 }
 
 for (j = i - 1; j >= 0; j--)
 {
 my_putchar(binary[j] + '0');
 }
+free(binary);
 }
 
